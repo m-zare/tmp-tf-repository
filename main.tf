76e54a43436e408f3b31cc4bf6c02c5918962327
m-zare <mostafazare@gmail.com>
2021-01-15 17:48:05 +0330

    root module

---
+++ main.tf
@@ -0,0 +1,14 @@
+module "sftp_server" {
+  source = "modules/sftp_server"
+
+  bucket = "sftp_agencies" 
+}
+
+module "sftp_user" {
+  source         = "modules/sftp_user"
+ 
+  bucket         = sftp_server.bucket
+  sftp_server_id = sftp_server.id
+  username       = "ftp_user"
+  ssh_key        = ""
+}
---
+++ variables.tf
@@ -0,0 +1,8 @@
+variable "region" {
+  default = "eu-west-1"
+}
+
+variable "username" {
+  type = string
+  description = "SFTP user"
+}
