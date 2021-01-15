eab1ba963bb1f3721b99d2c67dc9d13523d77284
m-zare <mostafazare@gmail.com>
2021-01-15 16:09:01 +0330

    create bucket

---
+++ aws-sftp/main.tf
@@ -0,0 +1,16 @@
+resource "aws_s3_bucket" "agencies" {
+  bucket = "agencies-sftp-tf"
+  versioning {
+    enabled = false
+  }
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+  tags = {
+    owner = "mzare"
+  }
+}
