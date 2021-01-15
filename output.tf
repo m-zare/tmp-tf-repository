15cf2774f6700ccc5753a3c66b2469ce37facc5e
m-zare <mostafazare@gmail.com>
2021-01-15 18:46:11 +0330

    output sftp_server detail

---
+++ output.tf
@@ -0,0 +1,14 @@
+output "id" {
+  value       = module.sftp_server.id
+  description = "The Transfer Server ID"
+}
+
+output "transfer_server_endpoint" {
+  value       = module.sftp_server.transfer_server_endpoint
+  description = "The endpoint of the Transfer Server"
+}
+
+output "bucket" {
+  value       = module.sftp_server.bucket
+  description = "SFTP bucket name"
+}
