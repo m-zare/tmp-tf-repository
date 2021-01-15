94ebf3046c804be895986847baa2629763b1bb78
m-zare <mostafazare@gmail.com>
2021-01-15 18:46:45 +0330

    correct variable names

--- modules/sftp_user/main.tf
+++ modules/sftp_user/main.tf
@@ -1,5 +1,5 @@
 resource "aws_iam_role" "sftp_role" {
-  name = "sftp_role"
+  name = "sftp_role_${var.username}"
   assume_role_policy = file("${path.module}/files/role.json")
   tags = {
     owner = "mzare"
@@ -7,11 +7,11 @@ resource "aws_iam_role" "sftp_role" {
 }
 
 locals {
-  policy = templatefile("${path.module}/templates/policy.tpl", { username = var.user1, bucket = var.bucket })
+  policy = templatefile("${path.module}/templates/policy.tpl", { username = var.username, bucket = var.bucket })
 }
 
 resource "aws_iam_role_policy" "sftp_policy" {
-  name = "sftp_iam_policy"
+  name = "sftp_iam_policy_${var.username}"
   role = aws_iam_role.sftp_role.id
   policy = local.policy
 }
@@ -20,7 +20,7 @@ resource "aws_transfer_user" "agency" {
   server_id = var.sftp_server_id
   user_name = var.username
   role      = aws_iam_role.sftp_role.arn
-  home_directory = format("/%s/%s", aws_s3_bucket.agencies.bucket, var.user)
+  home_directory = format("/%s/%s", var.bucket, var.username)
   tags = {
     owner = "mzare"
   }
