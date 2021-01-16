fc25f56c2c8ea3b76cf8dc147142edc747606251
m-zare <mostafazare@gmail.com>
2021-01-16 16:03:32 +0330

    correct role police

--- agencies_sftp/modules/notification/main.tf
+++ agencies_sftp/modules/notification/main.tf
@@ -17,21 +17,16 @@ resource "aws_iam_role_policy" "dynamodb-putitem" {
   policy = templatefile("${path.module}/templates/dynamodb-putitem.tpl", { table = aws_dynamodb_table.upload_history.arn })
 }
 
-data "aws_iam_policy" "AWSLambdaBasicExecutionRole" {
-  arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+resource "aws_iam_role_policy" "AWSLambdaBasicExecutionRole" {
+  name = "lambda-execution"
+  role = aws_iam_role.upload_history.id
+  policy = file("${path.module}/files/AWSLambdaBasicExecutionRole.json")
 }
 
 resource "aws_iam_role" "upload_history" {
   name = "upload-history-role"
   assume_role_policy = file("${path.module}/files/role.json")
-  policy = aws_iam_role_policy.dynamodb-putitem.policy
   tags = {
     owner = "mzare"
   }
-}
-
-resource "aws_iam_policy_attachment" "attach_policy" {
-  name       = "attach-AWSLambdaBasicExecutionRole"
-  roles      = ["${aws_iam_role.upload_history.name}"]
-  policy_arn = data.aws_iam_policy.AWSLambdaBasicExecutionRole.arn}"
 }
