b61def6e8bd21cccc4f0ca6b4d76e106a82a5589
m-zare <mostafazare@gmail.com>
2021-01-17 00:21:57 +0330

    cloudwatch and lambda permission

--- agencies_sftp/modules/notification/send_notification.tf
+++ agencies_sftp/modules/notification/send_notification.tf
@@ -10,10 +10,30 @@ resource "aws_lambda_function" "send_notification" {
   environment {
     variables = {
       Table = aws_dynamodb_table.upload_history.name
+      serverID = var.serverID
     }
   }
   
   tags = {
     owner = "mzare"
   }
+}
+
+resource "aws_cloudwatch_event_rule" "every_five_minutes" {
+  name = "every-five-minutes"
+  description = "Fires every five minutes"
+  schedule_expression = "rate(5 minutes)"
+}
+
+resource "aws_cloudwatch_event_target" "send_notification_every_five_minutes" {
+  rule = aws_cloudwatch_event_rule.every_five_minutes.name
+  arn = aws_lambda_function.send_notification.arn
+}
+
+resource "aws_lambda_permission" "allow_cloudwatch_to_call_send_notification" {
+    statement_id = "AllowExecutionFromCloudWatch"
+    action = "lambda:InvokeFunction"
+    function_name = aws_lambda_function.send_notification.function_name
+    principal = "events.amazonaws.com"
+    source_arn = aws_cloudwatch_event_rule.every_five_minutes.arn
 }
