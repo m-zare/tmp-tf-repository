53fe1832f566ee8b1395f82f7abf67c15e96429c
m-zare <mostafazare@gmail.com>
2021-01-17 00:37:26 +0330

    sftp server read policy

--- agencies_sftp/modules/notification/common.tf
+++ agencies_sftp/modules/notification/common.tf
@@ -10,6 +10,12 @@ resource "aws_iam_role_policy" "AWSLambdaBasicExecutionRole" {
   policy = file("${path.module}/files/AWSLambdaBasicExecution_role.json")
 }
 
+resource "aws_iam_role_policy" "sftp_server_readonly_policy" {
+  name = "sftp-server-read-only"
+  role = aws_iam_role.lambda_role.id
+  policy = file("${path.module}/files/sftp_server_readonly_policy.json")
+}
+
 resource "aws_iam_role" "lambda_role" {
   name = "lambda-role"
   assume_role_policy = file("${path.module}/files/lambda_role.json")
