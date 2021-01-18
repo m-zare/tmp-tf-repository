f97a3a397a182532108e110ee1ff8058a71e2704
m-zare <mostafazare@gmail.com>
2021-01-19 00:21:51 +0330

    remove defaults in main variables.tf

--- agencies_sftp/variables.tf
+++ agencies_sftp/variables.tf
@@ -5,36 +5,24 @@ variable "region" {
 variable "production" {
   description = "Production environment flag"
   type        = bool
-  default     = false
 }
 
 variable "username" {
   description = "SFTP user"
-
-  default = {
-    # # Read ssh key from string
-    # user1 = "ssh-rsa ..."
-    # # Read ssh key from file
-    # user2 = "~/sftp.pub"
-  }
 }
 
 variable "senderName" {
   description = "Name of the sender"
-  default     = "AWS SFTP Notification System"
 }
 
 variable "senderEmail" {
   description = "Sender email address"
-  default     = "mostafazare@gmail.com"
 }
 
 variable "recipientEmail" {
   description = "Recipient email address"
-  default     = "mostafazare@gmail.com"
 }
 
 variable "scheduleExpression" {
   description = "Cron expression for sending out emails"
-  default     = "cron(0 8 * * ? *)"
 }
